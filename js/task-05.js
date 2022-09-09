2f73b5927322c2df81ac870f700a3c58f6b2ea16
Valerii Droniak <droniak.valerii@gmail.com>
2022-09-09 19:32:27 +0300

    Add finished task-5

---
+++ js/task-05.js
@@ -0,0 +1,17 @@
+const ref = {
+    inputEl: document.querySelector('#name-input'),
+    outputEl: document.querySelector('#name-output')
+};
+
+ref.inputEl.addEventListener('input', onInputChange);
+ref.inputEl.addEventListener('blur', onInputblure);
+
+function onInputChange(event) {
+    ref.outputEl.textContent = event.currentTarget.value
+}
+
+function onInputblure(event) {
+    if (event.currentTarget.value === '') {
+        ref.outputEl.textContent = 'Anonymous';
+    }
+}
