ea9907bef309625e14cb2555043dda9038472e21
Valerii Droniak <droniak.valerii@gmail.com>
2022-09-10 14:43:04 +0300

    Add finished task-7

---
+++ js/task-07.js
@@ -0,0 +1,10 @@
+const inputEl = document.querySelector('#font-size-control');
+const textEl = document.querySelector('#text');
+
+inputEl.value = 16;
+
+inputEl.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    textEl.style.fontSize = event.currentTarget.value + 'px';
+}
